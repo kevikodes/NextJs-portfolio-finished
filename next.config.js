16961e3b5ada71180dfcce98faa554862816134d
Kevin Owens <33699674+kevikodes@users.noreply.github.com>
2022-08-12 12:31:00 -0700

    fixed

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/** @type {import('next').NextConfig} */ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     domains: ['harshgoel.me', 'i.ibb.co'],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
